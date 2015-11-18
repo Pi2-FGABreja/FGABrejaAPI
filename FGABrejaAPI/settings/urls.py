bd3e38aab5e43f723c9d5e9eb1b0023c505ffab4
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2015-11-18 11:20:38 -0200

    Added django project
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

---
+++ FGABrejaAPI/settings/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^auth/', include('authentication.urls')),
+    url(r'^', include('monitoring.urls')),
+]
