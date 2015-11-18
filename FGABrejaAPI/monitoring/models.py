7d0168c39f885da8f037499b3f09db9ccbbf06a4
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2015-11-18 11:37:37 -0200

    Added model
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- FGABrejaAPI/monitoring/models.py
+++ FGABrejaAPI/monitoring/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Sensor(models.Model):
+
+    sensor_type = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    position = models.CharField(max_length=50)
+
+    class Meta:
+        abstract = True
+
+
+class ThermalSensor(Sensor):
+
+    sensor_type = "thermal"
+    temperature = models.FloatField()
+
+
+class LevelSensor(Sensor):
+
+    sensor_type = "level"
+    level = models.CharField(max_length=50)
+
+
+class LDRSensor(Sensor):
+
+    sensor_type = "ldr"
+    luminosity = models.IntegerField()
