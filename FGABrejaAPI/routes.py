db624b19bdcae93f59e1ae000e0d6ed000a01860
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2015-11-18 11:10:26 -0200

    Removed flask project
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- FGABrejaAPI/routes.py
+++
@@ -1,8 +0,0 @@
-from FGABrejaAPI import app
-from FGABrejaAPI.views import SensorsView
-
-sensors_view = SensorsView.as_view('sensors_view')
-app.add_url_rule('/sensors/', defaults={'sensor_id': None},
-                 view_func=sensors_view, methods=['GET', ])
-app.add_url_rule('/sensors/<int:sensor_id>/',
-                 view_func=sensors_view, methods=['GET', ])
--- test.py
+++
@@ -1,8 +0,0 @@
-import pkgutil
-import unittest
-from FGABrejaAPI import tests
-
-for importer, modname, ispkg in pkgutil.iter_modules(tests.__path__):
-    exec('from FGABrejaAPI.tests.{} import *'.format(modname))
-
-unittest.main()
