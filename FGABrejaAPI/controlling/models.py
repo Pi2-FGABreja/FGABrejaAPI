c8bf149fc585d7e982b2a44d2a53ce2f22196ec1
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2015-11-25 19:19:11 -0200

    Initial models to controlling app
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- FGABrejaAPI/controlling/models.py
+++ FGABrejaAPI/controlling/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Heat(models.Model):
+    temperature = models.FloatField()
+    duration = models.IntegerField()
+
+
+class Hop(models.Model):
+    minutes = models.IntegerField()
+    weight = models.FloatField()
+
+
+class Recipe(models.Model):
+    heaters = models.ForeignKey('Heat')
+    hops = models.ForeignKey('Hop')
+
+
+class Process(models.Model):
+    initial_datetime = models.DateTimeField(auto_now=True)
+    final_datetime = models.DateTimeField(null=True)
+    recipe = models.ForeignKey('Recipe')
+    iodine_test = models.BooleanField(default=False)
+    malt = models.BooleanField(default=False)
