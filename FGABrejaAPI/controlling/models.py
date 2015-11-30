0e98e9acd4a523180a8ffc0d34ca6d05510b532d
Macário Soares <macario.junior@gmail.com>
2015-11-30 00:17:27 -0200

    Updating controlling model to get boiling temperature to boiling stage
    Signed-off-by: Macário Soares <macario.junior@gmail.com>

--- FGABrejaAPI/controlling/models.py
+++ FGABrejaAPI/controlling/models.py
@@ -23,7 +23,8 @@ class Valve(models.Model):
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=250)
-    boiling_temperature = models.FloatField()
+    initial_boiling_temperature = models.FloatField()
+    boiling_temperature = models.FloatField(default=97)
     malt = models.CharField(max_length=250)
     malt_weight = models.FloatField()
     hops_order = models.CharField(max_length=255, blank=True)
@@ -64,6 +65,10 @@ class Process(models.Model):
     actual_heat_time = models.DateTimeField(null=True)
     next_heat = models.IntegerField(default=2)
 
+    actual_hop = models.ForeignKey('Hop', null=True)
+    actual_hop_time = models.DateTimeField(null=True)
+    next_hop = models.IntegerField(default=2)
+
     filtering_init = models.DateTimeField(null=True)
 
     def change_heat(self):
@@ -72,6 +77,16 @@ class Process(models.Model):
         if delta >= timedelta(minutes=self.actual_heat.duration):
             return True
         elif self.actual_heat_time is None:
+            return True
+        else:
+            return False
+
+    def change_hop(self):
+        now = timezone.now()
+        delta = now - self.actual_hop_time
+        if delta >= timedelta(minutes=self.actual_hop.minutes):
+            return True
+        elif self.actual_hop_time is None:
             return True
         else:
             return False
