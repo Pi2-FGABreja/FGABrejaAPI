2efe1b37895a54788a68b6053cbd5657d52062a9
Macário Soares <macario.junior@gmail.com>
2015-12-02 01:00:31 -0200

    Serial comunication for brewery stage
    Signed-off-by: Macário Soares <macario.junior@gmail.com>
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- FGABrejaAPI/controlling/comunication.py
+++ FGABrejaAPI/controlling/comunication.py
@@ -6,15 +6,39 @@ class Comunication(object):
     def __init__(self):
         self.comunication_serial = serial.Serial(0)
 
+    # Pre Brewery Stage Serial Comunication
     def insert_water(self):
         self.comunication_serial.write("insert_water")
 
-    def get_filled_pot(self):
-        filled_pot = self.comunication_serial.readline()
-        if filled_pot == "reached":
-            self.stop_water()
-        else:
-            pass
+    def get_pot_level(self):
+        pot_level = self.comunication_serial.readline()
+        return pot_level
 
     def stop_water(self):
         self.comunication_serial.write("stop_water")
+
+    # Brewery Stage Serial Comunication
+    def turn_on_engine(self):
+        self.comunication_serial.write("turn_on_engine")
+
+    def get_engine_state(self):
+        engine_state = self.comunication_serial.readline()
+        return engine_state
+
+    def turn_on_resistor(self, pot, temperature=None):
+        if temperature is not None:
+            self.comunication_serial.write("turn_on_resistor_pot1 , "
+                                           "%.2f" % temperature)
+        else:
+            self.comunication_serial.write("turn_on_resistor_pot2")
+
+    def activate_alarm(self):
+        self.comunication_serial.write("activate_alarm")
+
+    def read_thermal_sensor(self, location):
+        self.comunication_serial.write("read_thermal_sensor %s" % location)
+        temperature = self.comunication_serial.readline()
+        return temperature
+
+    def end_stage(self, stage_name):
+        self.comunication_serial.write("End %s" % stage_name)
